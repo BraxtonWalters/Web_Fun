3010469d7bd852dd377ceaa07fed771a04c4c05b
Braxton Walters <braxton.walters@gmail.com>
2023-06-15 23:14:16 -0400

    finished making connections asgmt

--- JS/Walters_Braxton_Making_Connections/script.js
+++ JS/Walters_Braxton_Making_Connections/script.js
@@ -1,1 +1,22 @@
-console.log("page loaded...");
+
+
+function changeName(elemID) {
+    document.getElementById(elemID).innerText = "Braxton Walters";
+}
+
+function addUser(elemID0, elemID, elemID1) {
+    let connectionNum = parseInt(document.getElementById(elemID1).innerText);
+    let requestNum = parseInt(document.getElementById(elemID).innerText);
+    requestNum--;
+    connectionNum++;
+    document.getElementById(elemID1).innerText = connectionNum;
+    document.getElementById(elemID).innerText = requestNum;
+    document.getElementById(elemID0).remove();
+}
+
+function declineUser(elemID1, elemID2) {
+    let requestNum = parseInt(document.getElementById(elemID1).innerText);
+    requestNum--
+    document.getElementById(elemID1).innerText = requestNum;
+    document.getElementById(elemID2).remove();
+}
